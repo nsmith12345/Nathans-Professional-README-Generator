e93cac6a1f5fa1b849f6bde29e3434ba2d604f55
nsmith12345 <nsmith_2003@hotmail.com>
2022-04-19 12:05:16 -0400

    array of questions

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 // TODO: Include packages needed for this application
+Node.js 
+inquirer package 
+node_modules/
+.DS_Store/
+package.json    
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is your first name?", 
+    "What is your last name?", 
+    "What your were you born?"
+    ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
