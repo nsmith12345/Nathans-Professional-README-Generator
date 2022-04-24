7a026e00dae76fd18c07c46b5ccc83835403f293
nsmith12345 <nsmith_2003@hotmail.com>
2022-04-24 11:13:09 -0400

    functions for generateMarkdown

--- index.js
+++ index.js
@@ -119,10 +119,43 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = fileName => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./nathans-professional-README-generator.md', fileName, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'File created'
+            });
+        });
+    });
+}; 
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+
+    return inquirer.prompt(questions)
+    .then(readmeData => {
+        return readmeData;
+    })
+}
+
 
 // Function call to initialize app
-init();
+init() 
+.then(readmeData => {
+    console.log(readmeData);
+    return generateMarkdown(readmeData);
+})
+.then(pageMD => {
+    return fs.writeFile(pageMD);
+})
+.then(writeFileResponse => {
+    console.log(writeFileResponse.message);
+})
+.catch(err => {
+    console.log(err);
+})
